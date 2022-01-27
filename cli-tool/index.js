7b2b8cb75652da95d9143226bcbe69ca3e0fffa7
humam-hossain <humam.hossain.e@gmail.com>
2022-01-28 02:23:25 +0600

    updated on Fri Jan 28 02:23:25 AM +06 2022

---
+++ cli-tool/index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import gradient from 'gradient-string';
+import chalkAnimation from 'chalk-animation';
+import { createSpinner } from 'nanospinner';
+
+console.log(chalk.bgGreen('hi mom'));
