5072a6a8b9d5a55c4e7e9a3c3238990b2d0b4fc2
humam-hossain <humam.hossain.e@gmail.com>
2022-09-30 22:48:13 +0600

    module 8 started

--- nodejs/controllers/error.js
+++ nodejs/controllers/error.js
@@ -1,3 +1,3 @@
 exports.get404 = (req, res, next) => {
-    res.status(404).render('404', { docTitle: "404 - Page not found"});
+    res.status(404).render('404', { docTitle: "404 - Page not found", path: "/"});
 };
--- nodejs/models/product.js
+++ nodejs/models/product.js
@@ -3,14 +3,13 @@ const path = require('path');
 
 const p = path.join(path.dirname(process.mainModule.filename), "data", "products.json");
 
-const getProductsFromFile = cb => {    
+const getProductsFromFile = cb => {
     fs.readFile(p, (err, fileContent)=>{
         if(err){
            cb([]);
         }else{
             cb(JSON.parse(fileContent));
         }
-    
     });
 }
 
